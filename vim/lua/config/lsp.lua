f2626531fcd95e673b0b70d0ae929f3e4a3dbe96
Chiwan Park <chiwanpark@hotmail.com>
2023-01-17 14:28:31 +0900

    Apply gofmt and goimports before saving go source codes

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -52,8 +52,36 @@ function M.setup()
       on_attach(client, bufnr)
       -- disable exapndtab due to golang requires tab as indent
       vim.bo.expandtab = false
+      -- gofmt/goimports on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = {"*.go"},
+        callback = function()
+          vim.lsp.buf.format({
+            timeout_ms = 3000,
+          })
+
+          local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+          params.context = {only = {"source.organizeImports"}}
+
+          local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+          for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+              if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+              else
+                vim.lsp.buf.execute_command(r.command)
+              end
+            end
+          end
+        end
+      })
     end,
     capabilities = capabilities,
+    settings = {
+      gopls = {
+        ["local"] = "cheetah"
+      },
+    },
   }
   lspconfig["pylsp"].setup {
     on_attach = on_attach,
