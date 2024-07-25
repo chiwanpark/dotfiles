a4c1051bcfa640348a20cbc7d62d851ec93dc916
Chiwan Park <chiwanpark@hotmail.com>
2024-07-25 16:23:43 +0900

    Add basedpyright to LSP config for Python

--- vim/lua/config/lsp.lua
+++ vim/lua/config/lsp.lua
@@ -53,6 +53,11 @@ function M.setup()
     capabilities = capabilities,
   }
 
+  lspconfig["basedpyright"].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+
   lspconfig["gopls"].setup {
     on_attach = function(client, bufnr)
       on_attach(client, bufnr)
